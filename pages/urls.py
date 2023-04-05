de33b4f80e596341bf9ace562bfa26dc6c599dd8
Kurt Anjelo M. Sereño <kusereno@gbox.adnu.edu.ph>
2023-04-06 01:38:26 +0800

    Picdentify setup

---
+++ pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+app_name = 'pages'
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='admin'),
+]
